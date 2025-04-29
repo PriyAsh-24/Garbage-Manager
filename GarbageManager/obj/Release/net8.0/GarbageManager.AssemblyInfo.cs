4adeaa181eb9f12d302dd6b27a2c57f0355fe15d
Priyash Sharma <146582441+PriyAsh-24@users.noreply.github.com>
2025-04-29 12:30:07 +0530

    from inspace to SQLServer

--- GarbageManager/obj/Release/net8.0/GarbageManager.AssemblyInfo.cs
+++ GarbageManager/obj/Release/net8.0/GarbageManager.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarbageManager")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1a61c3cbe520a7ba5ba200b6cc5dde24170ba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarbageManager")]
